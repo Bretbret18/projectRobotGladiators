f56a4a87b4a2e6eb820bbf25f8b00ac494546c02
Bretbret18 <bret.sperry@gmail.com>
2021-11-08 14:17:38 -0500

    shop function skip function

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,9 +1,12 @@
-
+// Player Info
 var playerName;
 var playerHealth = 100;
 var playerAttack = 10;
-var playerMoney = 10;
+var playerMoney = 12;
 
+// // TO DO: 
+// - Make enemy values random
+// Enemy info array
 let enemyPlayer = [
     {
         name: 'Roborto',
@@ -22,8 +25,15 @@ let enemyPlayer = [
     }
 ];
 
+// current enemy iterator
 let enemyIterator = 0;
 
+if(playerHealth <= 0){
+    alert(`${playerName} has died. Game Over!`)
+    gameStart()
+}
+
+// Start Game function
 function gameStart() {
     alert('Welcome to Robot Gladiators!');
     playerName = window.prompt("What is your Robot's name?");
@@ -32,21 +42,105 @@ function gameStart() {
         // gameStart();
     } else {
         alert(playerName + ' has entered battle with '
-            + enemyPlayer[0].name + '!');
+            + enemyPlayer[enemyIterator].name + '!');
         confirmNext()
     }
 };
 
+// TO DO:
+// - Create different types of potions the player can buy
+// - Make sure playerMoney functions properly per buy
+// - player must be able to return to confirmNext()
+function shop() {
+    if (playerMoney <= 0) {
+        alert(`${playerName} has no $ left!`)
+        console.log(playerMoney);
+        confirmNext()
+    }
+    const shopChoice = prompt(`Welcome to the Shop! Would you
+     like to SHOP for a potion or RETURN to menu?`)
+    if (shopChoice === 'shop' || shopChoice === 'SHOP') {
+        const potionChoice = prompt(`Would you like to buy a
+        HEALTH potion or an ATTACK potion?`)
+        
+        if (potionChoice === 'health' || potionChoice === 'HEALTH') {
+            if(playerMoney <= 0){
+                alert(`${playerName} has no $ left!`)
+        console.log(playerMoney);
+        confirmNext()
+            }
+            playerHealth = playerHealth + 20;
+            playerMoney = playerMoney - 4;
+            console.log(playerHealth);
+            console.log(playerMoney);
+            alert(`Player health has increased to ${playerHealth}!`)
+            alert(`${playerName} now has ${playerMoney}$!`)
+            shop()
+        } else if (potionChoice === 'attack' || potionChoice === 'ATTACK') {
+            if(playerMoney <= 0){
+                alert(`${playerName} has no $ left!`)
+        console.log(playerMoney);
+        confirmNext()
+            }
+            playerAttack = playerAttack + 2;
+            playerMoney = playerMoney - 4;
+            console.log(playerAttack);
+            console.log(playerMoney);
+            alert(`Player attack has increased to ${playerAttack}!`)
+            alert(`${playerName} now has ${playerMoney}$!`)
+            shop()
+        } else {
+            alert(`You must choose a potion type!`)
+            shop()
+        }
+
+    } else if (shopChoice === 'return' || shopChoice === 'RETURN') {
+        confirmNext()
+    } else {
+        alert(`You must choose to either SHOP potions or
+         RETURN to main menu!`)
+        shop()
+    }
+}
+
+
+
+// Skip fight function
+function skip() {
+    // confirm player wants to skip round
+    let confirmSkip = confirm(
+        'Are you sure youd like to skip the round?');
+    if (confirmSkip === true || confirmSkip === 'true') {
+        alert(playerName +
+            ' has choosen to skip this round!');
+            
+        // deduct playerMoney from playerName
+        playerMoney = playerMoney - 8;
+        if(playerMoney <= 0) {
+            playerMoney = 0;
+        }
+        alert(playerName +
+            ' has the following amount of money left: '
+            + playerMoney)
+        console.log('playerMoney', playerMoney);
+        nextRound()
+    }
+};
+
+// Player Choice: fight, skip, or shop functions
 function confirmNext() {
-    let option = prompt('Would you like to FIGHT in the next round, SKIP the next round, or go to the fighter\'s SHOP?')
+    let option = prompt(`Would you like to FIGHT in the
+     next round, SKIP the next round, or go to
+      the fighter\'s SHOP?`)
     switch (option) {
         case 'fight' || 'FIGHT':
-           if(enemyPlayer[0].health === 0) {
-            alert(`${playerName} has entered battle with ${enemyPlayer[enemyIterator].name}!`);
-          fight()
+            if (enemyPlayer[0].health === 0) {
+                alert(`${playerName} has entered battle with
+             ${enemyPlayer[enemyIterator].name}!`);
+                fight()
             } else {
-            console.log(enemyPlayer[enemyIterator]);
-            option = fight();
+                console.log(enemyPlayer[enemyIterator]);
+                option = fight();
             }
             break;
         case 'skip' || 'SKIP':
@@ -55,91 +149,80 @@ function confirmNext() {
         case 'shop' || 'SHOP':
             option = shop();
             break;
+        case null:
+            option = gameStart()
     }
 
 };
 
+// Iterate to next enemy player
 function nextRound() {
     enemyIterator++
-    
     console.log(enemyPlayer[enemyIterator].name);
     confirmNext()
-}
+};
 
+// Fight function
 function fight() {
-    
-
-
     if (enemyPlayer[enemyIterator].health <= 0) {
         alert(enemyPlayer[enemyIterator].name + ' is dead!');
         nextRound()
     }
-
     console.log('fight');
-
-    
-
-    // Shows player attack on enemy and displays enemy Health
-    alert(playerName + ' has attacked ' + enemyPlayer[enemyIterator].name +
-        '! ' + enemyPlayer[enemyIterator].name + '\'s health is now ' +
+    alert(playerName + ' has attacked '
+        + enemyPlayer[enemyIterator].name +
+        '! ' + enemyPlayer[enemyIterator].name +
+        '\'s health is now ' +
         enemyPlayer[enemyIterator].health + ' !');
-    // Enemy health deducted by player attack
-    enemyPlayer[enemyIterator].health = enemyPlayer[enemyIterator].health - playerAttack;
-    // Show current enemy health
+
+    enemyPlayer[enemyIterator].health =
+        enemyPlayer[enemyIterator].health - playerAttack;
+
     console.log(enemyPlayer[enemyIterator].health);
 
-
-    // logs enemyPlayers new health
-    console.log(enemyPlayer[enemyIterator].name + '\'s health is now '
+    console.log(enemyPlayer[enemyIterator].name +
+        '\'s health is now '
         + enemyPlayer[enemyIterator].health + ' !');
-    // players new health after enemy attack
-    playerHealth = playerHealth - enemyPlayer[enemyIterator].attack;
-    if(playerHealth <= 0) {
+
+    playerHealth = playerHealth -
+        enemyPlayer[enemyIterator].attack;
+
+    if (playerHealth <= 0) {
         alert(`${playerName} has died! Game Over!`);
         console.log(`${playerName} has died! Game Over!`);
-        gameStart()
+        location.reload()
     }
-    // Shows enemyPlayer has attacked player,
-    // shows players new health post attack
-    alert(enemyPlayer[enemyIterator].name + ' has attacked ' + playerName +
-        '! ' + playerName + ' \'s health is now ' + playerHealth);
-    // logs players new health after attack
-    console.log(playerName + ' \'s health is now ' + playerHealth);
+
+    alert(enemyPlayer[enemyIterator].name +
+        ' has attacked ' + playerName +
+        '! ' + playerName + ' \'s health is now ' +
+        playerHealth);
+
+    console.log(playerName + ' \'s health is now ' +
+        playerHealth);
     console.log('end of set');
-    // continueFight variable prompts user to fight or skip
-    let continueFight = prompt('Would you like to FIGHT or SKIP this round?')
-    // Player either fights round or skips round
+
+    let continueFight = prompt
+        ('Would you like to FIGHT or SKIP this round?')
+
     if (continueFight === 'FIGHT' || continueFight === 'fight') {
         console.log('fight chosen');
         fight()
+
     } else if (continueFight === 'SKIP' || continueFight === 'skip') {
         enemyIterator++;
         console.log(enemyIterator);
         console.log('skip chosen');
         skip()
     }
+
 };
 
-function skip() {
-    // confirm player wants to skip round
-    let confirmSkip = confirm(
-        'Are you sure youd like to skip the round?');
-    if (confirmSkip === true || confirmSkip === 'true') {
-        alert(playerName +
-            ' has choosen to skip this round!');
-        // deduct playerMoney from playerName
-        playerMoney = playerMoney - 8;
-        alert(playerName +
-            ' has the following amount of money left: '
-            + playerMoney)
-        console.log('playerMoney', playerMoney);
-        confirmNext()
-    }
-}
+
 
 gameStart()
 
-console.log(enemyPlayer[0].name);
+
 
 
 
